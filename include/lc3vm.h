8e4c4239d1dd532ab97f8861f6eca8006b22224e
Brandon Halliday <BrandonHalliday75@gmail.com>
2026-04-14 21:52:18 +0000

    Implemented mem_read() and mem_write() functions
    Task 1 Test Passing

--- include/lc3vm.h
+++ include/lc3vm.h
@@ -1,9 +1,9 @@
 /** @file lc3vm.h
  * @brief LC-3 VM API
  *
- * @author Student Name
- * @note   cwid: 123456
- * @date   Spring 2024
+ * @author Brandon Halliday
+ * @note   cwid: 50376739
+ * @date   Spring 2026
  * @note   ide:  g++ 8.2.0 / GNU Make 4.2.1
  *
  * Header include file for LC-3 simulator API/functions.
@@ -65,7 +65,8 @@ extern uint16_t mem[];
 extern uint16_t reg[];
 extern uint16_t PC_START;
 
-// your task functions should go here
+uint16_t mem_read(uint16_t address);
+void mem_write(uint16_t address, uint16_t value);
 
 void rti(uint16_t i);
 void res(uint16_t i);
