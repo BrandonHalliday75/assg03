60247ca22226e7535d764f43e44b0e127924c031
Brandon Halliday <BrandonHalliday75@gmail.com>
2026-04-14 22:20:51 +0000

    Implemented st(), sti() & str() functions
    Task 7 Tests Passing

--- src/lc3vm.c
+++ src/lc3vm.c
@@ -312,7 +312,8 @@ void lea(uint16_t i)
  *   destination and source register operands, and to extract the
  *   second source register or the immediate value encoded in the
  */
-// put your implememtation of st() here below it documentation
+void st(uint16_t i)
+{ mem_write(reg[RPC] + PCOFF9(i), reg[DR(i)]); }
 
 /** @brief store indirect
  *
@@ -328,7 +329,8 @@ void lea(uint16_t i)
  *   destination and source register operands, and to extract the
  *   second source register or the immediate value encoded in the
  */
-// put your implememtation of sti() here below it documentation
+void sti(uint16_t i)
+{ mem_write(mem_read(reg[RPC] + PCOFF9(i)), reg[DR(i)]); }
 
 /** @brief store offset relative to base address
  *
@@ -343,7 +345,8 @@ void lea(uint16_t i)
  *   destination and source register operands, and to extract the
  *   second source register or the immediate value encoded in the
  */
-// put your implememtation of str() here below it documentation
+void str(uint16_t i)
+{ mem_write(reg[SR1(i)] + OFF6(i), reg[DR(i)]); }
 
 /** @brief jump unconditionally
  *
